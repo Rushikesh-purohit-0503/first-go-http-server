828541cdefe942a35218459fe349cba1eb21e073
Rushikesh-purohit-0503 <rushikesh.purohit.05@gmail.com>
2025-05-28 18:09:47 +0530

    added postgreSQl and use sqlc for query  generation

---
+++ handler_user.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/Rushikesh-purohit-0503/rssagg/internal/database"
+	"github.com/google/uuid"
+)
+
+func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Name string `json:"name"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+
+	params := parameters{}
+
+	if err := decoder.Decode(&params); err != nil {
+		responseWithError(w, 400, fmt.Sprintf("Invalid request payload : %v", err))
+		return
+	}
+
+	user, err := apiCfg.DB.CreateUser(r.Context(), database.CreateUserParams{
+		ID:        uuid.New(),
+		Name:      params.Name,
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+	})
+
+	if err != nil {
+		responseWithError(w, 400, fmt.Sprintf("Failed to create user: %v", err))
+		return
+	}
+	respondWithJSON(w, 201, databaseUserToUser(user))
+}
--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/Rushikesh-purohit-0503/rssagg/internal/database"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq" // PostgreSQL driver
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 
@@ -17,6 +24,18 @@ func main() {
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("PORT is not found in the environment")
+	}
+	dbUrl := os.Getenv("DB_URL")
+	if dbUrl == "" {
+		log.Fatal("DB_URL is not found in the environment")
+	}
+	conn, err:= sql.Open("postgres", dbUrl)
+	if err != nil {
+		log.Fatal("Error connecting to the database:", err)
+	}
+	
+	apiCfg := apiConfig{
+		DB: database.New(conn),
 	}
 	router := chi.NewRouter()
 	router.Use(
@@ -33,6 +52,7 @@ func main() {
 	v1Router := chi.NewRouter()
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/err", handleErrors)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
 	router.Mount("/v1", v1Router)
 
 	srv := &http.Server{
@@ -41,7 +61,7 @@ func main() {
 	}
 
 	log.Printf("Server running on port %v", portString)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatal("Error starting server:", err)
 	}
